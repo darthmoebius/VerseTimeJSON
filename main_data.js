d8f4d7a78a4d92a511dc89668b26f8d1db24d0e0
Moebi <85351662+darthmoebius@users.noreply.github.com>
2024-07-03 00:12:47 +0200

    new _data files  to test data only (currently only displaying settings window)

---
+++ main_data.js
@@ -0,0 +1,45 @@
+import Settings from './classes/app/Preferences.js';
+import DB from './classes/app/Database.js';
+import UI from './classes/app/UserInterface_data.js';
+
+window.suppressReload = false;
+
+function update() {
+	if (DB.locations.length === 0) return;
+	UI.update();
+}
+
+
+// INIT
+async function startVerseTime() {
+	await DB.createDatabase();
+	DB.locations.sort((a, b) => a.NAME.localeCompare(b.NAME));
+	UI.populateLocationList();
+	//UI.setupEventListeners();
+
+	//checkHash();
+	Settings.load();
+
+	setInterval(update, 250);
+	update();
+}
+startVerseTime();
+/*
+function checkHash() {
+	const hash = window.location.hash;
+	if (hash === '') return;
+
+	const hashParts = hash.replace('#', '').replaceAll('_', ' ').split('@');
+	const locationName = hashParts[0];
+
+	if (hashParts[1] !== undefined) {
+		UI.setCustomTime(hashParts[1], true);
+	}
+
+	UI.setMapLocation(locationName);
+}
+
+window.addEventListener('hashchange', () => {
+	if (window.suppressReload) return;
+	window.location.reload(true);
+}, false);*/
